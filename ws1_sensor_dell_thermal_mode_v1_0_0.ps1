c985dbf8e78d277951810e90ab4418d51519faf1
SvenRiebe <86112547+SvenRiebe@users.noreply.github.com>
2021-07-22 11:55:21 +0200

    Update ws1_sensor_dell_thermal_mode_v1_0_0.ps1

--- ws1_sensor_dell_thermal_mode_v1_0_0.ps1
+++ ws1_sensor_dell_thermal_mode_v1_0_0.ps1
@@ -1,4 +1,16 @@
-﻿$thermal_mode = switch(Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_ThermalInformation -Filter "AttributeName='Thermal Mode'" | Select -ExpandProperty CurrentValue)
+##################################################################
+#
+# Name: Sensor Dell BIOS Thermal Management Profile is set to device (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-21-2021
+
+$thermal_mode = switch(Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_ThermalInformation -Filter "AttributeName='Thermal Mode'" | Select -ExpandProperty CurrentValue)
 { 0 {"Optimized"}
   1 {"Cool"} 
   2 {"Quiet"}
