925ffd1565b39e20ffae4d18e9971f504d066d69
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 14:56:09 +0200

    Update ws1_sensor_device_cpu_v1_0_0.ps1

--- ws1_sensor_device_cpu_v1_0_0.ps1
+++ ws1_sensor_device_cpu_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$cpu_name = Get-WmiObject -class win32_processor | Select -ExpandProperty Name
+##################################################################
+#
+# Name: Sensor check cpu type in device
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$cpu_name = Get-WmiObject -class win32_processor | Select -ExpandProperty Name
 write-output $cpu_name
