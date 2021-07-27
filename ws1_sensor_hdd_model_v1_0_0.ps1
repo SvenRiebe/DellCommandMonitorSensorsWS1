3de51469bd518dc72e41cb21a4f2eb1ec6147e92
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:51:14 +0200

    Update ws1_sensor_hdd_model_v1_0_0.ps1

--- ws1_sensor_hdd_model_v1_0_0.ps1
+++ ws1_sensor_hdd_model_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$hdd_modelnumber = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_SMARTAttributeInfo -Filter "AttributeName='vendor-specific'" | Select -ExpandProperty ModelNumber
+##################################################################
+#
+# Name: Sensor for HDD Model ID
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$hdd_modelnumber = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_SMARTAttributeInfo -Filter "AttributeName='vendor-specific'" | Select -ExpandProperty ModelNumber
 write-output $hdd_modelnumber
