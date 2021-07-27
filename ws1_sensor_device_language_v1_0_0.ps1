db5910875fe8421babaaa387612ff23b0e0033a1
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:14:35 +0200

    Update ws1_sensor_device_language_v1_0_0.ps1

--- ws1_sensor_device_language_v1_0_0.ps1
+++ ws1_sensor_device_language_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$os_languages = (Get-WinUserLanguageList)[0].LocalizedName
+##################################################################
+#
+# Name: Sensor Win10 language setting on device
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$os_languages = (Get-WinUserLanguageList)[0].LocalizedName
 Write-Output $os_languages
