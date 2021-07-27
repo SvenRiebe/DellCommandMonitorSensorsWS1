0d11dbe7afb4224eda4dfe904870d8034dbe553a
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 13:46:03 +0200

    Update ws1_sensor_dell_oem_model_v1_0_0.ps1

--- ws1_sensor_dell_oem_model_v1_0_0.ps1
+++ ws1_sensor_dell_oem_model_v1_0_0.ps1
@@ -1,2 +1,15 @@
-﻿$oem_model = Get-CimInstance -Namespace root\dcim\sysman -ClassName dcim_Chassis -Filter "Name='Main System Chassis'" | select -ExpandProperty Model
+  
+##################################################################
+#
+# Name: Sensor Dell Notebook Model Name (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$oem_model = Get-CimInstance -Namespace root\dcim\sysman -ClassName dcim_Chassis -Filter "Name='Main System Chassis'" | select -ExpandProperty Model
 Write-Output $oem_model
