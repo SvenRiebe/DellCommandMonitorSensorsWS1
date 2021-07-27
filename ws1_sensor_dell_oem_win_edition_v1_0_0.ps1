88574567cbf46ce3b8c08a3b7a343e6a242214a7
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 14:31:00 +0200

    Update ws1_sensor_dell_oem_win_edition_v1_0_0.ps1

--- ws1_sensor_dell_oem_win_edition_v1_0_0.ps1
+++ ws1_sensor_dell_oem_win_edition_v1_0_0.ps1
@@ -1,2 +1,13 @@
-$oem_win_edition = Get-CimInstance -namespace root\cimv2 -classname SoftwareLicensingService | Select-Object OA3xOriginalProductKeyDescription
+##################################################################
+#
+# Name: Sensor Win10 OEM Version who is bought with the device
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$oem_win_edition = Get-CimInstance -namespace root\cimv2 -classname SoftwareLicensingService | Select-Object -ExpandProperty OA3xOriginalProductKeyDescription
 Write-Output $oem_win_edition
