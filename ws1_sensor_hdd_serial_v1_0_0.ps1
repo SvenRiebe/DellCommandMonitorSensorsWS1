bef87a402b4065e864cd6395cddefcdc22dc15ab
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:52:47 +0200

    Update ws1_sensor_hdd_serial_v1_0_0.ps1

--- ws1_sensor_hdd_serial_v1_0_0.ps1
+++ ws1_sensor_hdd_serial_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$hdd_serialnumber = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_SMARTAttributeInfo -Filter "AttributeName='vendor-specific'" | Select -ExpandProperty SerialNumber
+##################################################################
+#
+# Name: Sensor for HDD serial no
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$hdd_serialnumber = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_SMARTAttributeInfo -Filter "AttributeName='vendor-specific'" | Select -ExpandProperty SerialNumber
 write-output $hdd_serialnumber
