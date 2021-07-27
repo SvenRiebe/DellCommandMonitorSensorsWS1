f0c1763ac695aaa4d63008ea9d726cb7cecb263e
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:32:35 +0200

    Update ws1_sensor_ftp_reshorizontal_v1_0_0.ps1

--- ws1_sensor_ftp_reshorizontal_v1_0_0.ps1
+++ ws1_sensor_ftp_reshorizontal_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$monitor_reshorizontal = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_FlatPanel | Select -ExpandProperty HorizontalResolution
+##################################################################
+#
+# Name: Sensor Dell Notebook Flat panel horizontal resolution setting actual (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$monitor_reshorizontal = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_FlatPanel | Select -ExpandProperty HorizontalResolution
 write-output $monitor_reshorizontal
