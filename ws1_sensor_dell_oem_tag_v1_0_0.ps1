4981bf96a425195fcdb23916e09a436e04fbd221
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 14:07:03 +0200

    Update ws1_sensor_dell_oem_tag_v1_0_0.ps1

--- ws1_sensor_dell_oem_tag_v1_0_0.ps1
+++ ws1_sensor_dell_oem_tag_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$servicetag = Get-WmiObject -class win32_bios | Select -ExpandProperty SerialNumber
+##################################################################
+#
+# Name: Sensor Device Serialnumber/ServiceTag
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$servicetag = Get-WmiObject -class win32_bios | Select -ExpandProperty SerialNumber
 write-output $servicetag
