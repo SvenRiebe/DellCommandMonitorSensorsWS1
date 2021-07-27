8e329e7e48d507ded64116dd74daabcba602b659
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 10:30:44 +0200

    Update ws1_sensor_battery_health_v1_0_0.ps1

--- ws1_sensor_battery_health_v1_0_0.ps1
+++ ws1_sensor_battery_health_v1_0_0.ps1
@@ -1,4 +1,16 @@
-﻿$battery_healthstate = Switch (Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_Battery | Select -ExpandProperty HealthState)
+##################################################################
+#
+# Name: Sensor Dell Notebook Battery health (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$battery_healthstate = Switch (Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_Battery | Select -ExpandProperty HealthState)
 {
     0 {"Unknown"}
     5 {"OK"}
