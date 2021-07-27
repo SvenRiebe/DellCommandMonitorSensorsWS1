e5a419d80de26e8daab93f9fdcb88732abada08a
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 14:17:11 +0200

    Update ws1_sensor_dell_oem_licensekey_v1_0_0.ps1

--- ws1_sensor_dell_oem_licensekey_v1_0_0.ps1
+++ ws1_sensor_dell_oem_licensekey_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$oem_licencekey = (Get-WmiObject -query 'select * from SoftwareLicensingService‘).OA3xOriginalProductKey
+##################################################################
+#
+# Name: Sensor for Win10 Licensekey stored in BIOS
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$oem_licencekey = (Get-WmiObject -query 'select * from SoftwareLicensingService‘).OA3xOriginalProductKey
 Write-Output $oem_licencekey
