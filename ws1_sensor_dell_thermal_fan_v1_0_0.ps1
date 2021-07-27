66603d1436643bb5bf33855427aee17e751fb487
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 14:36:00 +0200

    Update ws1_sensor_dell_thermal_fan_v1_0_0.ps1

--- ws1_sensor_dell_thermal_fan_v1_0_0.ps1
+++ ws1_sensor_dell_thermal_fan_v1_0_0.ps1
@@ -1,4 +1,16 @@
-﻿$thermal_fan = switch(Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_ThermalInformation -Filter "AttributeName='Fan Failure Mode'" | Select -ExpandProperty CurrentValue)
+##################################################################
+#
+# Name: Sensor Dell Notebook Fan Health Status (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$thermal_fan = switch(Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_ThermalInformation -Filter "AttributeName='Fan Failure Mode'" | Select -ExpandProperty CurrentValue)
 { 0 {"Catastrophic Fan Failure"}
   1 {"Minimal Fan Failure"} 
   2 {"No Failure"}
