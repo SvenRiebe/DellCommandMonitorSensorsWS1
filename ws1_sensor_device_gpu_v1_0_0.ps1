10a073c9c0e35e0d4e439f13c94b4e422abeb13f
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 14:59:17 +0200

    Update ws1_sensor_device_gpu_v1_0_0.ps1

--- ws1_sensor_device_gpu_v1_0_0.ps1
+++ ws1_sensor_device_gpu_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$gpu_name = Get-WmiObject -class win32_VideoController | Select -ExpandProperty Caption
+##################################################################
+#
+# Name: Sensor for GPU type in device
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$gpu_name = Get-WmiObject -class win32_VideoController | Select -ExpandProperty Caption
 write-output $gpu_name
