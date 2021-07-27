85da22ab946677c05391fd2c4a47d19d20346eec
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:10:51 +0200

    Update ws1_sensor_device_name_v1_0_0.ps1

--- ws1_sensor_device_name_v1_0_0.ps1
+++ ws1_sensor_device_name_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$cpu_name = Get-WmiObject -class win32_processor | Select -ExpandProperty Name
-write-output $cpu_name
+##################################################################
+#
+# Name: Sensor for Device Name
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$device_name = Get-CimInstance Win32_Computersystem | select -ExpandProperty Name
+write-output $device_name
