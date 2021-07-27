c2071e9584d410973ddc846eaae6a39d73f022a6
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:20:33 +0200

    Update ws1_sensor_ftp_brightness_v1_0_0.ps1

--- ws1_sensor_ftp_brightness_v1_0_0.ps1
+++ ws1_sensor_ftp_brightness_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$monitor_brightness = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_FlatPanel | Select -ExpandProperty Brightness
+##################################################################
+#
+# Name: Sensor Dell Notebook Flat panel brightness (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$monitor_brightness = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_FlatPanel | Select -ExpandProperty Brightness
 write-output $monitor_brightness
