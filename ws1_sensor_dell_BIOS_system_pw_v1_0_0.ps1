d56d366a4c2db5ef827f4504878bc470354ae27e
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 11:20:25 +0200

    Update ws1_sensor_dell_BIOS_system_pw_v1_0_0.ps1

--- ws1_sensor_dell_BIOS_system_pw_v1_0_0.ps1
+++ ws1_sensor_dell_BIOS_system_pw_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$bios_systempwd = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_BIOSPassword -filter "AttributeName='SystemPwd'"| Select -ExpandProperty IsSet
+##################################################################
+#
+# Name: Sensor Dell check if in BIOS the system pw is set (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$bios_systempwd = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_BIOSPassword -filter "AttributeName='SystemPwd'"| Select -ExpandProperty IsSet
 write-output $bios_systempwd
