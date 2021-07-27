b7f7b6264aa3e2cec680ef1979e408bacad5400a
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:41:50 +0200

    Update ws1_sensor_hdd_boot_fw_v1_0_0.ps1

--- ws1_sensor_hdd_boot_fw_v1_0_0.ps1
+++ ws1_sensor_hdd_boot_fw_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$hdd_fw = Get-WmiObject -class win32_DiskDrive -Filter "Partitions='3'" | Select -ExpandProperty FirmwareRevision
+##################################################################
+#
+# Name: Sensor for HDD firmware ID
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$hdd_fw = Get-WmiObject -class win32_DiskDrive -Filter "Partitions='3'" | Select -ExpandProperty FirmwareRevision
 write-output $hdd_fw
