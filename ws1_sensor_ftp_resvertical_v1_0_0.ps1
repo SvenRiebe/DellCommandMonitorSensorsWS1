88c021f2975cd584c71f75126135ea4f90821fff
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:37:17 +0200

    Update ws1_sensor_ftp_resvertical_v1_0_0.ps1

--- ws1_sensor_ftp_resvertical_v1_0_0.ps1
+++ ws1_sensor_ftp_resvertical_v1_0_0.ps1
@@ -1,2 +1,15 @@
-﻿$monitor_resvertical = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_FlatPanel | Select -ExpandProperty VerticalResolution
+##################################################################
+#
+# Name: Sensor Dell Notebook Flat panel vertical resolution setting actual (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+
+$monitor_resvertical = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_FlatPanel | Select -ExpandProperty VerticalResolution
 write-output $monitor_resvertical
