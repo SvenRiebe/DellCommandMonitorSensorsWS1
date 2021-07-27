78d5b0dac2f1e13f2abccbc07e7ac14516f26dc7
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 11:18:43 +0200

    Update ws1_sensor_dell_BIOS_version_v1_0_0.ps1

--- ws1_sensor_dell_BIOS_version_v1_0_0.ps1
+++ ws1_sensor_dell_BIOS_version_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$bios_version = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_BIOSElement | Select -ExpandProperty Version
+##################################################################
+#
+# Name: Sensor Dell BIOS Version (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$bios_version = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_BIOSElement | Select -ExpandProperty Version
 write-output $bios_version
