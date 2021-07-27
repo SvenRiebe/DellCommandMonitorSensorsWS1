c6dcdf7c25396bd4a9c983d2617ce3bfd881b7d2
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-27 15:46:51 +0200

    Update ws1_sensor_hdd_bootdisk_v1_0_0.ps1

--- ws1_sensor_hdd_bootdisk_v1_0_0.ps1
+++ ws1_sensor_hdd_bootdisk_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$hdd_model = Get-WmiObject -class win32_DiskDrive -Filter "Partitions='3'" | Select -ExpandProperty model
+##################################################################
+#
+# Name: Sensor for HDD Model
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$hdd_model = Get-WmiObject -class win32_DiskDrive -Filter "Partitions='3'" | Select -ExpandProperty model
 write-output $hdd_model
