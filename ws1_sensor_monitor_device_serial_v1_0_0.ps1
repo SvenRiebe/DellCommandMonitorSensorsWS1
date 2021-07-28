3477e3f2a730544be3377c14311bad6129765876
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-28 14:15:42 +0200

    Update ws1_sensor_monitor_device_serial_v1_0_0.ps1

--- ws1_sensor_monitor_device_serial_v1_0_0.ps1
+++ ws1_sensor_monitor_device_serial_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$monitor_oemdevice = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_DesktopMonitor -Filter "Caption='Root/MainSystemChassis/DisplayAdapterObj/DisplayObj:0'" | Select -ExpandProperty SystemName
+##################################################################
+#
+# Name: Sensor for identifiy attached first display
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$monitor_oemdevice = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_DesktopMonitor -Filter "Caption='Root/MainSystemChassis/DisplayAdapterObj/DisplayObj:0'" | Select -ExpandProperty SystemName
 write-output $monitor_oemdevice
