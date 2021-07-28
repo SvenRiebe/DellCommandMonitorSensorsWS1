1e302754b9fe2aee6b5bde5d646f2b466c52aa6b
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-28 14:28:55 +0200

    Update ws1_sensor_monitor_model_v1_0_0.ps1

--- ws1_sensor_monitor_model_v1_0_0.ps1
+++ ws1_sensor_monitor_model_v1_0_0.ps1
@@ -1,2 +1,13 @@
-﻿$monitor_model = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_DesktopMonitor -Filter "Caption='Root/MainSystemChassis/DisplayAdapterObj/DisplayObj:0'" | Select -ExpandProperty Description
+##################################################################
+#
+# Name: Sensor for Dell Monitor Model primary display
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$monitor_model = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_DesktopMonitor -Filter "Caption='Root/MainSystemChassis/DisplayAdapterObj/DisplayObj:0'" | Select -ExpandProperty Description
 write-output $monitor_model
