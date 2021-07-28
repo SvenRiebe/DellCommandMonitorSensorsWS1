504dbb93a06e81d94bb15b1a180b73bda31f1ff6
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-07-28 14:59:08 +0200

    Update ws1_sensor_monitor_health_v1_0_0.ps1

--- ws1_sensor_monitor_health_v1_0_0.ps1
+++ ws1_sensor_monitor_health_v1_0_0.ps1
@@ -1,4 +1,15 @@
-﻿$monitor_healthstate = Switch (Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_DesktopMonitor -Filter "Caption='Root/MainSystemChassis/DisplayAdapterObj/DisplayObj:0'" | Select -ExpandProperty HealthState)
+##################################################################
+#
+# Name: Sensor for health state of first attached display (need Dell Command Monitor on the Client)
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$monitor_healthstate = Switch (Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_DesktopMonitor -Filter "Caption='Root/MainSystemChassis/DisplayAdapterObj/DisplayObj:0'" | Select -ExpandProperty HealthState)
 {
     0 {"Unknown"}
     5 {"OK"}
