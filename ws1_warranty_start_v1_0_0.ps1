b4da3081d9c6ae6b9f2f384c3dca98c1e83acee8
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-03 11:46:57 +0200

    Update ws1_warranty_start_v1_0_0.ps1

--- ws1_warranty_start_v1_0_0.ps1
+++ ws1_warranty_start_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$warranty_start = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_AssetWarrantyInformation | Select -ExpandProperty WarrantyStartDate
+##################################################################
+#
+# Name: Sensor Dell warrenty start date information (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$warranty_start = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_AssetWarrantyInformation | Select -ExpandProperty WarrantyStartDate
 write-output $warranty_start[0]
