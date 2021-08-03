7248a9fdd4fe31439d4c32ac69ef84426a993a39
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-03 11:39:57 +0200

    Update ws1_sensor_vpro_smx_v1_0_0.ps1

--- ws1_sensor_vpro_smx_v1_0_0.ps1
+++ ws1_sensor_vpro_smx_v1_0_0.ps1
@@ -1,4 +1,16 @@
-﻿$vpro_smx = Switch(Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_VProSettings | Select -ExpandProperty SMXState)
+##################################################################
+#
+# Name: Sensor Dell check SMX status in BIOS (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$vpro_smx = Switch(Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_VProSettings | Select -ExpandProperty SMXState)
 {
     2 {"Enabled"}
     3 {"Disabled"}
