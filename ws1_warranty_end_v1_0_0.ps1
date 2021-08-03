f691c6c26381d353284a18591b719dfba855fc4a
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-03 11:46:30 +0200

    Update ws1_warranty_end_v1_0_0.ps1

--- ws1_warranty_end_v1_0_0.ps1
+++ ws1_warranty_end_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$warranty_end = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_AssetWarrantyInformation -f| Select -ExpandProperty WarrantyEndDate
+##################################################################
+#
+# Name: Sensor Dell warrenty end date information (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$warranty_end = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_AssetWarrantyInformation -f| Select -ExpandProperty WarrantyEndDate
 write-output $warranty_end[0]
