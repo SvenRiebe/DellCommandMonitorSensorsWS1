4efc348a845321fe64f7c8377e980aab840e3823
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-03 11:12:32 +0200

    Update ws1_sensor_ram_manufacturer_v1_0_0.ps1

--- ws1_sensor_ram_manufacturer_v1_0_0.ps1
+++ ws1_sensor_ram_manufacturer_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$ram_manufacturer = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_PhysicalMemory | Select -ExpandProperty Manufacturer
+##################################################################
+#
+# Name: Sensor Dell check who is RAM manufacturer (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$ram_manufacturer = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_PhysicalMemory | Select -ExpandProperty Manufacturer
 write-output $ram_manufacturer[0]
