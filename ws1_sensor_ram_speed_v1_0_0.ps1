c79b80d651b50eedb5af3c018b50a7ed8fbb704b
Sven Riebe @Dell <86112547+SvenRiebe@users.noreply.github.com>
2021-08-03 11:24:18 +0200

    Update ws1_sensor_ram_speed_v1_0_0.ps1

--- ws1_sensor_ram_speed_v1_0_0.ps1
+++ ws1_sensor_ram_speed_v1_0_0.ps1
@@ -1,2 +1,14 @@
-﻿$ram_speed = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_PhysicalMemory | Select -ExpandProperty Speed
+##################################################################
+#
+# Name: Sensor Dell check speed of RAM Modules (need Dell Command Monitor on the device)
+#
+# Author: Sven Riebe
+#
+# Status: validate
+#
+# Version 1.0.0
+#
+# Date: 07-06-2021
+
+$ram_speed = Get-CimInstance -Namespace root\dcim\sysman -ClassName DCIM_PhysicalMemory | Select -ExpandProperty Speed
 write-output $ram_speed[0]
